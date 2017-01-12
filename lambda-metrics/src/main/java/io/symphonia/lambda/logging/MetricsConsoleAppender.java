3dbbca9b53d11b59ef2c31abb99e20b9627615b1
John Chapin <john.chapin@gmail.com>
2017-01-12 17:38:55 -0500

    Remove AWSRequestId from the default logback configs

--- lambda-metrics/src/main/java/io/symphonia/lambda/logging/MetricsConsoleAppender.java
+++ lambda-metrics/src/main/java/io/symphonia/lambda/logging/MetricsConsoleAppender.java
@@ -4,7 +4,7 @@ public class MetricsConsoleAppender extends LambdaConsoleAppender {
 
     @SuppressWarnings("WeakerAccess")
     public static String DEFAULT_METRICS_PATTERN =
-            "[%d{yyyy-MM-dd HH:mm:ss.SSS}] %X{AWSRequestId:-NO-REQUEST-ID} METRIC %logger{5} %replace(%msg){'[,= ]+', ' '}%n";
+            "[%d{yyyy-MM-dd HH:mm:ss.SSS}] METRIC %logger{5} %replace(%msg){'[,= ]+', ' '}%n";
 
     public MetricsConsoleAppender() {
         super(DEFAULT_METRICS_PATTERN, new MetricPassFilter());
--- lambda-slf4j/src/main/java/io/symphonia/lambda/logging/DefaultConsoleAppender.java
+++ lambda-slf4j/src/main/java/io/symphonia/lambda/logging/DefaultConsoleAppender.java
@@ -6,7 +6,7 @@ public class DefaultConsoleAppender extends LambdaConsoleAppender {
     public static String NO_REQUEST_ID = "NO-REQUEST-ID";
 
     public static String DEFAULT_PATTERN =
-            "[%d{yyyy-MM-dd HH:mm:ss.SSS}] %X{AWSRequestId:-" + NO_REQUEST_ID + "} %.-6level %logger{5} - %msg%n";
+            "[%d{yyyy-MM-dd HH:mm:ss.SSS}] %.-6level %logger{5} - %msg%n";
 
     public DefaultConsoleAppender() {
         super(DEFAULT_PATTERN);
